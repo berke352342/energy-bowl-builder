f44318ed780430af2851cbe8b40da13401b121cd
Ahmet Berke Carkci <berkcark4@gmail.com>
2026-03-16 10:32:00 -0400

    Berke: added EnergyBowl class with price and calories calculation

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-class EnergyBowl  {
+class EnergyBowl{
 
-constructor(base, fruits, size){
+constructor(base, fruits, size) {
 
-this.base = base;
-this.fruits = fruits;
-this.size = size;
+ this.base = base;
+ this.fruits = fruits;
+ this.size = size;
+
+}
+
+calculatePrice(){
+
+  let price = 5; //  base price
+
+  price += this.fruits.length * 2;
+
+if(this.size === "Medium") price += 2;
+if(this.size === "Large") price += 4;
+
+return price;
+
+}
+
+  calculateCalories(){
+
+let calories = 200;
+
+calories += this.fruits.length * 50;
+
+ if(this.size === "Medium") calories += 100;
+ if(this.size === "Large") calories += 200;
+
+return calories;
 
 }
 
@@ -13,35 +39,37 @@ getDescription(){
 return "Your Energy Bowl:<br>" +
 "Base: " + this.base + "<br>" +
 "Fruits: " + this.fruits.join(", ") + "<br>" +
-"Size: " + this.size;
-
-     }
+"Size: " + this.size + "<br><br>" +
+"Total Price: $" + this.calculatePrice() + "<br>" +
+"Total Calories: " + this.calculateCalories() + " kcal";
 
 }
 
-  const button = document.getElementById("calculateBtn");
-  const result = document.getElementById("result");
+}
 
-button.addEventListener("click", function(){
+const button = document.getElementById("calculateBtn");
+const result = document.getElementById("result");
+
+button.addEventListener("click", function() {
 
 const base = document.getElementById("base").value;
 
-const fruitCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-let fruits = [];
+  const fruitCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  let fruits = [];
 
-fruitCheckboxes.forEach(function(cb){
-fruits.push(cb.value);
-});
+ fruitCheckboxes.forEach(function(cb){
+ fruits.push(cb.value);
+  });
 
-  const sizeRadio = document.querySelector('input[name="size"]:checked');
-  let size = "";
+const sizeRadio = document.querySelector('input[name="size"]:checked');
+let size = "";
 
 if(sizeRadio){
-size = sizeRadio.value;
-   }
+ size = sizeRadio.value;
+}
 
  const bowl = new EnergyBowl(base, fruits, size);
 
-    result.innerHTML = bowl.getDescription();
+ result.innerHTML = bowl.getDescription();
 
-  });
+});
